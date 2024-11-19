bef0210377f49e29370e3d8b7a1e5d5b8d08962e
simfrosagradiani123 <simfrosagradiani@gmail.com>
2024-11-19 22:02:57 +0700

    Add files via upload

---
+++ Latihan1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1><b>Belajar PHP Dasar</b></h1>
+    <?php
+        echo "Hello World<br>"; // Tidak cetak tebal
+    ?>
+
+    <h2><b>Menggunakan Variabel</b></h2>
+    <?php
+        // Deklarasi variabel
+        $nim = "312310748";
+        $nama = "Simfrosa Gradiani";
+
+        // Output dengan variabel
+        echo "NIM: " . $nim . "<br>";
+        echo "Nama: " . $nama;
+    ?>
+</body>
+</html>
